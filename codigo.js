fc7c704208fabaa5906d56764fb4da0f9f5b40f0
SouVitorGabriel <vitor.s.alencar@hotmail.com>
2024-06-04 14:13:32 -0300

    adicionado reconhecimento do botão enter para enviar a mensagem; Caixa de texto limpa ao enviar a mensagem.

--- codigo.js
+++ codigo.js
@@ -2,8 +2,15 @@ let caixaDeTexto = document.querySelector("#mensagem");
 
 let caixaDeResposta = document.querySelector("#resposta");
 
+caixaDeTexto.onkeydown = function(event){
+  if(event.key === 'Enter') {
+      enviar();        
+  }
+}
+
 function enviar() {
   let textoUser = "<hr/> <p>" + caixaDeTexto.value + "</p>";
+  caixaDeTexto.value = "";
   let respostaBot;
 
   if (caixaDeTexto.value === "oi") {
